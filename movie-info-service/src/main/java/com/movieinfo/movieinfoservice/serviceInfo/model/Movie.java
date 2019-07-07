dfb8d7fc4a8c602cc3a720da54d3dcf1b0221f5f
mzoui <mah_zouihed@yahoo.fr>
2019-07-07 13:27:26 -0400

    movie info service

---
+++ movie-info-service/src/main/java/com/movieinfo/movieinfoservice/serviceInfo/model/Movie.java
@@ -0,0 +1,27 @@
+package com.movieinfo.movieinfoservice.serviceInfo.model;
+
+public class Movie {
+	private String moveiId;
+	private String movieName;
+	
+	
+	public Movie(String moveiId, String movieName) {
+		super();
+		this.moveiId = moveiId;
+		this.movieName = movieName;
+	}
+	public String getMoveiId() {
+		return moveiId;
+	}
+	public void setMoveiId(String moveiId) {
+		this.moveiId = moveiId;
+	}
+	public String getMovieName() {
+		return movieName;
+	}
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+	
+
+}
